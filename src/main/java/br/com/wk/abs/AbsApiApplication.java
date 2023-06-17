9ef267185bfdf5eaaabdbaf64c886eaedafadd74
Rogerio <rogeriosilva60@gmail.com>
2023-06-17 09:46:11 -0300

    initial project

---
+++ src/main/java/br/com/wk/abs/AbsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.wk.abs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wk/abs/AbsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wk.abs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
