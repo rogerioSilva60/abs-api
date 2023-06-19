7b274493ec9bea0f119adc37ecb1bfa5d34b815f
Rogerio <rogeriosilva60@gmail.com>
2023-06-19 16:09:36 -0300

    Adjusted to work with the mysql database

--- src/main/java/br/com/wk/abs/repositories/UsuarioRepository.java
+++ src/main/java/br/com/wk/abs/repositories/UsuarioRepository.java
@@ -16,30 +16,30 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>, Usuario
   @Query("select new br.com.wk.abs.vo.CandidatoVO(count(u), u.endereco.estado) from Usuario u group by u.endereco.estado")
   List<CandidatoVO> buscarAgrupadoPorEstado();
 
-  @Query("select new br.com.wk.abs.vo.UsuarioImcVO(u.nome, datediff(yy, u.dataNascimento, current_date) as idade, "
-      + "(u.peso/(u.altura * u.altura)) as imc) from Usuario u order by datediff(yy, u.dataNascimento, current_date) asc")
+  @Query("select new br.com.wk.abs.vo.UsuarioImcVO(u.nome, (datediff(current_date, u.dataNascimento) / 365) as idade, "
+      + "(u.peso/(u.altura * u.altura)) as imc) from Usuario u order by (datediff(current_date, u.dataNascimento) / 365) asc")
   List<UsuarioImcVO> buscarIMCPorUsuario();
 
-  @Query("select new br.com.wk.abs.vo.UsuarioImcVO(u.nome, datediff(yy, u.dataNascimento, current_date) as idade, "
-      + "(u.peso/(u.altura * u.altura)) as imc) from Usuario u where u.genero=?1 "
-      + "order by datediff(yy, u.dataNascimento, current_date) asc")
+  @Query("select new br.com.wk.abs.vo.UsuarioImcVO(u.nome, (datediff(current_date, u.dataNascimento) / 365) as idade, (u.peso/(u.altura * u.altura)) as imc) "
+      + "from Usuario u where u.genero=?1 "
+      + "order by (datediff(current_date, u.dataNascimento) / 365)")
   List<UsuarioImcVO> buscarIMCDoUsuarioPorGenero(Genero genero);
 
-  @Query("select new br.com.wk.abs.vo.UsuarioImcVO(datediff(yy, u.dataNascimento, current_date) as idade, "
-      + "(u.peso/(u.altura * u.altura)) as imc) from Usuario u where datediff(yy, u.dataNascimento, current_date) between ?1 and ?2 "
-      + "order by datediff(yy, u.dataNascimento, current_date) asc")
+  @Query("select new br.com.wk.abs.vo.UsuarioImcVO((datediff(current_date, u.dataNascimento) / 365) as idade, "
+      + "(u.peso/(u.altura * u.altura)) as imc) from Usuario u where (datediff(current_date, u.dataNascimento) / 365) between ?1 and ?2 "
+      + "order by (datediff(current_date, u.dataNascimento) / 365) asc")
   List<UsuarioImcVO> buscarIMCPorIdade(int menroIdade, int maiorIdade);
 
-  @Query("select new br.com.wk.abs.vo.GrupoSanguineoVO(u.tipoSanguineo, (sum(datediff(yy, u.dataNascimento, current_date)) / count(u.tipoSanguineo))) from Usuario u "
+  @Query("select new br.com.wk.abs.vo.GrupoSanguineoVO(u.tipoSanguineo, (sum((datediff(current_date, u.dataNascimento) / 365)) / count(u.tipoSanguineo))) from Usuario u "
       + "group by u.tipoSanguineo order by u.tipoSanguineo asc")
   List<GrupoSanguineoVO> buscarMediaIdadePorTipoSanguineo();
 
   @Query("select count(u) from Usuario u where u.tipoSanguineo in (?1) "
-      + "and datediff(yy, u.dataNascimento, current_date) between ?2 and ?3 and u.peso > ?4")
+      + "and (datediff(current_date, u.dataNascimento) / 365) between ?2 and ?3 and u.peso > ?4")
   Long buscarTotalDoaresPorTipoSanguineo(List<TipoSanguineo> tipoSanguineos,
       Integer menorIdade, Integer maiorIdade, Integer peso);
 
-  @Query("select max(datediff(yy, u.dataNascimento, current_date)) as maior_idade from Usuario u")
+  @Query("select max((datediff(current_date, u.dataNascimento) / 365)) as maior_idade from Usuario u")
   Integer buscarMaiorIdade();
 
 }
