06431910c24a15e1befde90bfb29abc8a13ff45f
rogerio <rogeriosilva60@gmail.com>
2023-06-20 14:42:52 -0300

    Added test to handle BusinessException exception when trying to save a batch of users

--- src/test/java/br/com/wk/abs/services/impl/UsuarioServiceImplTest.java
+++ src/test/java/br/com/wk/abs/services/impl/UsuarioServiceImplTest.java
@@ -2,12 +2,18 @@ package br.com.wk.abs.services.impl;
 
 import static br.com.wk.abs.mother.UsuarioMother.startUsuario;
 import static br.com.wk.abs.mother.UsuarioMother.usuarios;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import br.com.wk.abs.repositories.UsuarioRepository;
 import br.com.wk.abs.services.UsuarioService;
+import br.com.wk.abs.services.exceptions.BusinessException;
+import java.util.ArrayList;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -37,6 +43,15 @@ class UsuarioServiceImplTest {
     verify(repository, times(1)).salvarEmLote(usuarios);
   }
 
+  @Test
+  void givenUsuariosWhenSalvarEmLoteThenReturnBusinessException() {
+    doThrow(new RuntimeException()).when(repository).salvarEmLote(any());
 
+    Throwable exception = Assertions.catchThrowable(() -> service.salvarEmLote(new ArrayList<>()));
+
+    assertThat(exception)
+        .isInstanceOf(BusinessException.class)
+        .hasMessage("Ocorreu um erro ao salvar usuários");
+  }
 
 }
